24a6ec3ddc5fbc1171bfd708fb4f787cd0a01d70
D.Edwards <Demarcus.edwards@bison.howard.edu>
2020-12-08 14:08:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -124,9 +124,9 @@ col3.header("Adversarial")
 
 
 file_type = 'jpg'
+slider = st.select_slider('Slide to select', options=['Default','Upload Your own'])
 
-button = st.button('Use Default Image')
-if button:
+if slider == 1:
     uploaded_file = 'Ways-To-Keep-Your-Parrot-Happy-And-Healthy.jpg'
 else:
     uploaded_file = st.file_uploader("Choose an Image!",type = file_type)
